5ac4b7f6f69514974f0f95d0570137cd18860340
Angel Medina <astre369@gmail.com>
2015-03-20 17:00:34 -0600

    Se adiciona la biblioteca de Google Maps Se adicionar un helper para UIColor Se agrega el color y icono de TaxiTaxi

---
+++ TaxiTaxiiOS/TaxiTaxiiOS/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiTaxiiOS
+//
+//  Created by Angel Medina on 20/03/15.
+//  Copyright (c) 2015 Angel Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = UIColor(red:38,green:194,blue:129)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
