534154cbaa86363ddff4da8a7587304a6ef91b4e
Wirwing <irving.cf@gmail.com>
2015-03-20 18:24:37 -0500

    Se agrega activity para seleccionar destino.

--- android/Taxi/app/src/main/java/com/mobile/taxi/SelectDestinyActivity.java
+++ android/Taxi/app/src/main/java/com/mobile/taxi/SelectDestinyActivity.java
@@ -6,13 +6,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class SelectDestinyActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_select_destiny);
 
         // Replace action bar with toolbar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -21,11 +20,10 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_select_destiny, menu);
         return true;
     }
 
