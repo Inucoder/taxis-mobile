e5092aaa8b14c2511e1dbab439b3efdf2d5e8efb
Wirwing <irving.cf@gmail.com>
2015-03-21 07:15:17 -0500

    Se agrega evento para obtener datos de un punto latlng.

---
+++ android/Taxi/app/src/main/java/com/mobile/taxi/events/GeocodeLatLngEvent.java
@@ -0,0 +1,17 @@
+package com.mobile.taxi.events;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Irving on 21/03/2015.
+ */
+public class GeocodeLatLngEvent {
+
+    public final LatLng point;
+
+    public GeocodeLatLngEvent(LatLng point) {
+        this.point = point;
+    }
+
+
+}
